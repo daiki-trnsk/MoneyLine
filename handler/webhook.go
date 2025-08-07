10a53761961ad23cd54eb8de00c7b1d497e488cb
daiki <dahuizhusong@gmail.com>
2025-08-07 21:22:31 +0900

    メンションテスト

--- handler/webhook.go
+++ handler/webhook.go
@@ -3,16 +3,12 @@ package handler
 import (
 	"log"
 	"net/http"
-	"regexp"
-	"strconv"
-	"strings"
+	"os"
+	// "strconv"
+	// "strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
-)
-
-var (
-	mentionPattern = regexp.MustCompile(`@([^\s]+)\s*@([^\s]+)\s*(\d+)\s*(.*)`)
 )
 
 func WebhookHandler(bot *linebot.Client) echo.HandlerFunc {
@@ -29,36 +25,44 @@ func WebhookHandler(bot *linebot.Client) echo.HandlerFunc {
 					continue
 				}
 
-				// メンションが2人（Bot + 債務者）含まれているか確認
-				if msg.Mention == nil || len(msg.Mention.Mentionees) < 2 {
-					log.Println("メンションが足りない or 無効")
+				// マネリンがメンションされるか
+				botUserID := os.Getenv("MONEYLINE_BOT_ID")
+				found := false
+				for _, m := range msg.Mention.Mentionees {
+					if m.UserID == botUserID {
+						found = true
+						break
+					}
+				}
+				if !found {
+					log.Println("Bot自身がメンションされていないのでスキップ")
 					continue
 				}
 
 				// 債権者（送信者）
-				creditorID := event.Source.UserID
+				// creditorID := event.Source.UserID
 
-				// 債務者
-				debtorID := msg.Mention.Mentionees[1].UserID
+				// // 債務者
+				// debtorID := msg.Mention.Mentionees[1].UserID
 
-				// 金額とメモを抽出
-				tokens := strings.Fields(msg.Text)
-				if len(tokens) < 3 {
-					log.Println("金額やメモが足りません")
-					continue
-				}
+				// // 金額とメモを抽出
+				// tokens := strings.Fields(msg.Text)
+				// if len(tokens) < 3 {
+				// 	log.Println("金額やメモが足りません")
+				// 	continue
+				// }
 
-				amount, err := strconv.Atoi(tokens[2])
-				if err != nil {
-					log.Println("金額が数値でない:", tokens[2])
-					continue
-				}
+				// amount, err := strconv.Atoi(tokens[2])
+				// if err != nil {
+				// 	log.Println("金額が数値でない:", tokens[2])
+				// 	continue
+				// }
 
-				memo := strings.Join(tokens[3:], " ")
+				// memo := strings.Join(tokens[3:], " ")
 
-				log.Printf("債権者（送信者）: %s, 債務者（メンション）: %s, 金額: %d, メモ: %s", creditorID, debtorID, amount, memo)
+				// log.Printf("債権者（送信者）: %s, 債務者（メンション）: %s, 金額: %d, メモ: %s", creditorID, debtorID, amount, memo)
 
-				if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("記録しました！")).Do(); err != nil {
+				if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("メンションされました！")).Do(); err != nil {
 					log.Println("返信エラー:", err)
 				}
 			}
