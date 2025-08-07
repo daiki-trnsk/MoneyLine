04878e396f4089856f824acc55c659cb3ee2be5e
daiki <dahuizhusong@gmail.com>
2025-08-07 21:14:59 +0900

    main

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,6 +8,8 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
+	
+	"github.com/daiki-trnsk/MoneyLine/handler"
 )
 
 func main() {
@@ -17,35 +18,18 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	
-    bot, err := linebot.New(
-        os.Getenv("LINE_CHANNEL_SECRET"),
-        os.Getenv("LINE_CHANNEL_TOKEN"),
-    )
-    if err != nil {
-        log.Fatal("Error initializing LINE bot:", err)
-    }
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal("Error initializing LINE bot:", err)
+	}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "MoneyLine Server is running!")
 	})
 
-    e.POST("/webhook", func(c echo.Context) error {
-        events, err := bot.ParseRequest(c.Request())
-        if err != nil {
-            return c.NoContent(http.StatusBadRequest)
-        }
-
-        for _, event := range events {
-            if event.Type == linebot.EventTypeMessage {
-                if msg, ok := event.Message.(*linebot.TextMessage); ok {
-					fmt.Printf("moneyline userid: %s\n", msg.Mention.Mentionees[0].UserID)
-                    bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("受信したメッセージ: "+msg.Text)).Do()
-                }
-            }
-        }
-
-        return c.String(http.StatusOK, "ok")
-    })
+	e.POST("/webhook", handler.WebhookHandler(bot))
 	e.Logger.Fatal(e.Start(":8000"))
 }
