ae0f175b3d4a2f6f5e51d09a2c0ed92afb18ca2c
daiki <dahuizhusong@gmail.com>
2025-08-08 18:32:07 +0900

    ログ出力

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	// "github.com/line/line-bot-sdk-go/v7/linebot"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 
-	// "github.com/daiki-trnsk/MoneyLine/handler"
+	"github.com/daiki-trnsk/MoneyLine/handler"
 )
 
 func main() {
@@ -18,19 +18,26 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// bot, err := linebot.New(
-	// 	os.Getenv("LINE_CHANNEL_SECRET"),
-	// 	os.Getenv("LINE_CHANNEL_TOKEN"),
-	// )
-	// if err != nil {
-	// 	log.Fatal("Error initializing LINE bot:", err)
-	// }
+	
+	log.Println("✅ MoneyLine main() 入りました")
+
+	// 環境変数チェック
+	log.Println("LINE_CHANNEL_SECRET:", os.Getenv("LINE_CHANNEL_SECRET"))
+	log.Println("PORT:", os.Getenv("PORT"))
+
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal("Error initializing LINE bot:", err)
+	}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "MoneyLine Server is running!")
 	})
 
-	// e.POST("/webhook", handler.WebhookHandler(bot))
+	e.POST("/webhook", handler.WebhookHandler(bot))
 
 	port := os.Getenv("PORT")
 	if port == "" {
