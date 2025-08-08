09da4eded260b33b02801e7fdf7d984b6e7a8bd4
daiki <dahuizhusong@gmail.com>
2025-08-08 18:21:00 +0900

    デプロイできない

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/line/line-bot-sdk-go/v7/linebot"
+	// "github.com/line/line-bot-sdk-go/v7/linebot"
 
-	"github.com/daiki-trnsk/MoneyLine/handler"
+	// "github.com/daiki-trnsk/MoneyLine/handler"
 )
 
 func main() {
@@ -18,23 +18,24 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	bot, err := linebot.New(
-		os.Getenv("LINE_CHANNEL_SECRET"),
-		os.Getenv("LINE_CHANNEL_TOKEN"),
-	)
-	if err != nil {
-		log.Fatal("Error initializing LINE bot:", err)
-	}
+	// bot, err := linebot.New(
+	// 	os.Getenv("LINE_CHANNEL_SECRET"),
+	// 	os.Getenv("LINE_CHANNEL_TOKEN"),
+	// )
+	// if err != nil {
+	// 	log.Fatal("Error initializing LINE bot:", err)
+	// }
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "MoneyLine Server is running!")
 	})
 
-	e.POST("/webhook", handler.WebhookHandler(bot))
+	// e.POST("/webhook", handler.WebhookHandler(bot))
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
 	}
+	log.Println("Starting server on port:", port)
 	log.Fatal(e.Start(":" + port))
 }
