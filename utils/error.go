7220383b998dd1658468b82816b7a2cb048595e8
daiki <dahuizhusong@gmail.com>
2025-09-02 22:02:47 +0900

    エラーメール通知

--- utils/error.go
+++ utils/error.go
@@ -17,9 +17,20 @@ func LogAndReplyError(err error, in dto.Incoming, title string) linebot.SendingM
 		loc = fmt.Sprintf("%s:%d", file, line)
 	}
 
+	// ログ出力
 	log.Printf("[ERROR] title=%s err=%v caller=%s incoming=%+v",
 		title, err, loc, in,
 	)
+
+	// メール通知
+	subject := fmt.Sprintf("[ERROR] %s", title)
+	body := fmt.Sprintf(
+		"Error occurred:\n\nTitle: %s\nError: %v\nLocation: %s\nIncoming: %+v",
+		title, err, loc, in,
+	)
+	if emailErr := SendEmail(subject, body); emailErr != nil {
+		log.Printf("[ERROR] Failed to send error notification email: %v", emailErr)
+	}
 
 	return linebot.NewTextMessage("エラーが発生しました。")
 }
